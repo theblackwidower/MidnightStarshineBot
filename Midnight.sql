3b264008e3030176590220a60832f0effe635900
T Duke Perry <theblackwidower@noprestige.com>
2019-12-12 23:21:08 -0500

    Added the licence boilerplate
    Added the licence boilerplate to the top of the two code files in the 
    project. Should've done this much sooner, if I'm honest.

--- Midnight.sql
+++ Midnight.sql
@@ -1,3 +1,21 @@
+	-- ------------------------------------------------------------------------
+	-- MidnightStarshineBot - a multipurpose Discord bot
+	-- Copyright (C) 2019  T. Duke Perry
+	--
+	-- This program is free software: you can redistribute it and/or modify
+	-- it under the terms of the GNU Affero General Public License as published
+	-- by the Free Software Foundation, either version 3 of the License, or
+	-- (at your option) any later version.
+	--
+	-- This program is distributed in the hope that it will be useful,
+	-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+	-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	-- GNU Affero General Public License for more details.
+	--
+	-- You should have received a copy of the GNU Affero General Public License
+	-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+	-- ------------------------------------------------------------------------
+
 CREATE TABLE "tbl_currency" (
 	"server_id"	INTEGER NOT NULL,
 	"member_id"	INTEGER NOT NULL,
