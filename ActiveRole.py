4d1fed31be7f3dacc615e09edd19c0db478a05d2
T Duke Perry <theblackwidower@noprestige.com>
2022-03-30 22:41:03 -0400

    Fixed copyright dates
    I realized that the copyright dates shouldn't just reflect when the file was last updated, but when it was created as well. And I haven't been doing that as I updated the files this year. So, I fixed that.

--- ActiveRole.py
+++ ActiveRole.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------
 #      MidnightStarshineBot - a multipurpose Discord bot
-#      Copyright (C) 2022  T. Duke Perry
+#      Copyright (C) 2020-2022  T. Duke Perry
 #
 #      This program is free software: you can redistribute it and/or modify
 #      it under the terms of the GNU Affero General Public License as published
